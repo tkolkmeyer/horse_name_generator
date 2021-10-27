f9f75a1d3b5d754b4ea4273abb02d2fa2558166f
Taylor Kolkmeyer <tkolkmeyer@gmail.com>
2021-10-27 14:35:17 -0500

    asdf

--- generator.js
+++ generator.js
@@ -1,50 +1,71 @@
+window.addEventListener("load", function() {
+
+    let form = document.querySelector("form");
+    form.addEventListener("submit", function(event) {
+
+        let name = document.querySelector("input[name=name").value;
+
+        if (name.value === "") {
+            alert("All fields are required!");
+        };
+
+        document.getElementById('hello-you').innerHTML = `That's great, ${name.value} . . . we're gonna call you ____`;
+
+        event.preventDefault();
+
+    });
+
+});
+// example
+
+
 // function generator() {
-let form = document.getElementById('name-form');
-form.addEventListener("submit-button", function(event) {
+// let form = document.getElementById('name-form');
+// form.addEventListener("submit-button", function(event) {
 
-            let name = document.getElementById("name").value;
-            document.getElementById('hello-you').innerHTML = `That's great, ${name.value} . . . we're gonna call you ____`;
-        }
+//             let name = document.getElementById("name").value;
+//             document.getElementById('hello-you').innerHTML = `That's great, ${name.value} . . . we're gonna call you ____`;
+//         }
 
-        // // add single syllable common names and adjectives
-        // let randomBimboFirst = ["disco", "ginger", "cherry"];
+// // add single syllable common names and adjectives
+// let randomBimboFirst = ["disco", "ginger", "cherry"];
 
-        // // add nouns and shit
-        // let randomBimboLast = ["mama", "banana cake", "dipshit"];
+// // add nouns and shit
+// let randomBimboLast = ["mama", "banana cake", "dipshit"];
 
-        // let firstNameIndex = Math.floor(Math.random() * (randomBimboFirst.length));
-
-
-        // let lastNameIndex =
-        //     Math.floor(Math.random() * (randomBimboLast.length));
-
-        // let firstName = randomBimboFirst[firstNameIndex];
-
-        // let lastName = randomBimboLast[lastNameIndex];
-
-        // // what's your name?
-        // // thats great . . . we're going to call you _____
+// let firstNameIndex = Math.floor(Math.random() * (randomBimboFirst.length));
 
 
+// let lastNameIndex =
+//     Math.floor(Math.random() * (randomBimboLast.length));
 
-        // return "Your bimbo name is: " + firstName + " " + lastName;
-        // }
+// let firstName = randomBimboFirst[firstNameIndex];
 
-        // window.addEventListener("load", function() {
+// let lastName = randomBimboLast[lastNameIndex];
 
-        //     let form = document.querySelector("form");
-        //     form.addEventListener("submit", function(event) {
+// // what's your name?
+// // thats great . . . we're going to call you _____
 
-        //         let name = document.getElementById("name");
 
-        //         if (name.value === "") {
-        //             alert("All fields are required!");
-        //         };
 
-        //         document.getElementById('hello-you').innerHTML = `That's great, ${name.value} . . . we're gonna call you ____`;
+// return "Your bimbo name is: " + firstName + " " + lastName;
+// }
 
-        //         event.preventDefault();
+// window.addEventListener("load", function() {
 
-        //     });
+//     let form = document.querySelector("form");
+//     form.addEventListener("submit", function(event) {
 
-        // });
+//         let name = document.getElementById("name");
+
+//         if (name.value === "") {
+//             alert("All fields are required!");
+//         };
+
+//         document.getElementById('hello-you').innerHTML = `That's great, ${name.value} . . . we're gonna call you ____`;
+
+//         event.preventDefault();
+
+//     });
+
+// });
