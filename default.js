5668601325be363f9b1079a2752cc0ca0da862bb
Taylor Kolkmeyer <tkolkmeyer@gmail.com>
2021-10-27 03:15:14 -0500

    wrapped

--- default.js
+++ default.js
@@ -1,30 +1,3 @@
-function spinLogo(x) {
+const generator = require('./bimbo-time.js');
 
-    x.classList.add("spin");
-}
-
-function generator() {
-
-    const input = require('readline-sync');
-
-    let info = input.question("What is your name? ");
-
-
-    // add single syllable common names and adjectives
-    let randomBimboFirst = ["disco", "ginger", "cherry"];
-
-    // add nouns and shit
-    let randomBimboLast = ["mama", "banana cake", "dipshit"];
-
-    let firstNameIndex = Math.floor(Math.random() * (randomBimboFirst.length));
-
-
-    let lastNameIndex =
-        Math.floor(Math.random() * (randomBimboLast.length));
-
-    let firstName = randomBimboFirst[firstNameIndex];
-
-    let lastName = randomBimboLast[lastNameIndex];
-
-    return "Your bimbo name is: " + firstName + " " + lastName;
-}
+generator.runProgram();
