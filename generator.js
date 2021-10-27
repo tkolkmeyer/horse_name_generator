0a5c274ae52f6665efd4da6bfdb068c0dc3d8abd
Taylor Kolkmeyer <tkolkmeyer@gmail.com>
2021-10-27 13:42:22 -0500

    asdf

--- generator.js
+++ generator.js
@@ -1,29 +1,48 @@
-function generator() {
-    let name = document.getElementById("name").value;
+// function generator() {
+//     let name = document.getElementById("name").value;
 
-    console.log(name);
-
-
-    // // add single syllable common names and adjectives
-    // let randomBimboFirst = ["disco", "ginger", "cherry"];
-
-    // // add nouns and shit
-    // let randomBimboLast = ["mama", "banana cake", "dipshit"];
-
-    // let firstNameIndex = Math.floor(Math.random() * (randomBimboFirst.length));
+//     console.log(name);
 
 
-    // let lastNameIndex =
-    //     Math.floor(Math.random() * (randomBimboLast.length));
+// // add single syllable common names and adjectives
+// let randomBimboFirst = ["disco", "ginger", "cherry"];
 
-    // let firstName = randomBimboFirst[firstNameIndex];
+// // add nouns and shit
+// let randomBimboLast = ["mama", "banana cake", "dipshit"];
 
-    // let lastName = randomBimboLast[lastNameIndex];
-
-    // // what's your name?
-    // // thats great . . . we're going to call you _____
+// let firstNameIndex = Math.floor(Math.random() * (randomBimboFirst.length));
 
 
+// let lastNameIndex =
+//     Math.floor(Math.random() * (randomBimboLast.length));
 
-    // return "Your bimbo name is: " + firstName + " " + lastName;
-}
+// let firstName = randomBimboFirst[firstNameIndex];
+
+// let lastName = randomBimboLast[lastNameIndex];
+
+// // what's your name?
+// // thats great . . . we're going to call you _____
+
+
+
+// return "Your bimbo name is: " + firstName + " " + lastName;
+// }
+
+window.addEventListener("load", function() {
+
+    let form = document.querySelector("form");
+    form.addEventListener("submit", function(event) {
+
+        let name = document.getElementById("name");
+
+        if (name.value === "") {
+            alert("All fields are required!");
+        };
+
+        document.getElementById('hello-you').innerHTML = `That's great, ${name.value} . . . we're gonna call you ____`;
+
+        event.preventDefault();
+
+    });
+
+});
