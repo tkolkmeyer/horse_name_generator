46e249da8b9d78cd4c736d400d759838d9ea1272
tkolkmeyer <tkolkmeyer@gmail.com>
2026-05-24 13:10:01 -0500

    udpated styles

--- generator.js
+++ generator.js
@@ -260,7 +260,7 @@ window.addEventListener("load", function() {
         const lastHorseName = lastName[lastNameIndex];
         const horseName = `${firstHorseName} ${lastHorseName}`;
 
-        document.getElementById('hello-you').innerHTML = `Your horse's name is: ${horseName}`;
+        document.getElementById('hello-you').innerHTML = horseName;
     });
 
 });
